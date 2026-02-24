39661c432636e6402f10fb25470bff71673500be
Jraiden <hai.phu@gmail.com>
2026-02-24 21:51:29 +0700

    v8: fetch+browser fallback, URL normalize, Console.log debug

--- uukanshu-cc/src/gen.js
+++ uukanshu-cc/src/gen.js
@@ -1,16 +1,41 @@
 function execute(url, page) {
     if (!page) page = '1';
-    var fullUrl = "https://www.uukanshu.cc" + url.replace(/_\d+\.html$/, "_" + page + ".html");
 
-    var browser = Engine.newBrowser();
-    var doc = browser.launch(fullUrl, 15000);
-    browser.close();
+    var BASE = "https://www.uukanshu.cc";
+    var fullUrl = url.startsWith("http") ? url : BASE + url;
+    fullUrl = fullUrl.replace(/_\d+\.html$/, "_" + page + ".html");
+
+    Console.log("gen.js fullUrl: " + fullUrl);
+
+    // Try fetch first
+    var doc = null;
+    try {
+        var response = fetch(fullUrl);
+        if (response.ok) {
+            var html = response.text();
+            Console.log("gen.js html length: " + html.length);
+            if (html.length > 1000 && html.indexOf("bookbox") > -1) {
+                doc = Html.parse(html);
+            }
+        }
+    } catch (e) {
+        Console.log("gen.js fetch failed: " + e);
+    }
+
+    // Fallback to browser if fetch failed or blocked
+    if (!doc) {
+        Console.log("gen.js using browser fallback");
+        var browser = Engine.newBrowser();
+        doc = browser.launch(fullUrl, 15000);
+        browser.close();
+    }
 
     if (!doc) return Response.success([], null);
 
     var el = doc.select(".bookbox");
-    var data = [];
+    Console.log("gen.js bookbox count: " + el.size());
 
+    var data = [];
     for (var i = 0; i < el.size(); i++) {
         var e = el.get(i);
         var nameEl = e.select(".bookname a").first();
@@ -20,16 +45,18 @@ function execute(url, page) {
         var author = authorEl ? authorEl.text() : "";
 
         if (name && link) {
+            if (!link.startsWith("http")) link = BASE + link;
             data.push({
                 name: name,
                 link: link,
                 cover: "",
                 description: author,
-                host: "https://www.uukanshu.cc"
+                host: BASE
             });
         }
     }
 
+    Console.log("gen.js results: " + data.length);
     var next = data.length > 0 ? (parseInt(page) + 1).toString() : null;
     return Response.success(data, next);
 }
